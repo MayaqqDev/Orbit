bc7ff57b6eab8faf9c8ea3fe916f7ad60b15c068
Mayaqq <i@mayaqq.dev>
2025-03-26 22:11:02 +0100

    renamed some variables

--- src/main/kotlin/dev/mayaqq/orbit/screen/GlobalConfigScreen.kt
+++ src/main/kotlin/dev/mayaqq/orbit/screen/GlobalConfigScreen.kt
@@ -5,6 +5,7 @@ import dev.mayaqq.orbit.utils.McClient
 import dev.mayaqq.orbit.utils.Text
 import earth.terrarium.olympus.client.components.Widgets
 import earth.terrarium.olympus.client.components.renderers.WidgetRenderers
+import earth.terrarium.olympus.client.components.textbox.TextBox
 import earth.terrarium.olympus.client.layouts.Layouts
 import earth.terrarium.olympus.client.ui.UIConstants
 import earth.terrarium.olympus.client.utils.State
@@ -12,26 +13,27 @@ import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.layouts.HeaderAndFooterLayout
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.CommonComponents
-import net.minecraft.network.chat.Style
 import java.awt.Color
 import kotlin.math.min
 
 class GlobalConfigScreen(val previousScreen: Screen? = null) : Screen(Text.trans("config.orbit.global")) {
+    private val layout: HeaderAndFooterLayout = HeaderAndFooterLayout(this)
+    private var buttonCount = OrbitConfig.CONFIG.buttonCount
 
-    val layout: HeaderAndFooterLayout = HeaderAndFooterLayout(this)
-    var buttonCount = OrbitConfig.CONFIG.buttonCount
-    val state = State.of(buttonCount)
-    val slider = Widgets.intInput(state)
+    private val buttonCountState: State<Int> = State.of(buttonCount)
+    private val buttonCountWidget: TextBox = Widgets.intInput(buttonCountState)
 
     override fun init() {
         layout.addTitleHeader(this.title, McClient.font)
-        val column = Layouts.column()
-        val numInput = Layouts.row()
 
-        slider.withPlaceholder("8")
-        slider.withSize(20, 20)
+        val configColumn = Layouts.column()
 
-        numInput.withChild(
+        val buttonCountRow = Layouts.row()
+
+        buttonCountWidget.withPlaceholder("8")
+        buttonCountWidget.withSize(20, 20)
+
+        buttonCountRow.withChild(
             Widgets.text(
                 Text.trans("config.orbit.global.button_count").withColor(Color.WHITE.rgb)
             )
@@ -39,10 +41,12 @@ class GlobalConfigScreen(val previousScreen: Screen? = null) : Screen(Text.trans
                 .withShadow()
                 .withSize(180, 20)
         )
-        numInput.withChild(slider)
-        column.withChild(numInput)
+        buttonCountRow.withChild(buttonCountWidget)
 
-        layout.addToContents(column)
+        configColumn.withChild(buttonCountRow)
+
+        layout.addToContents(configColumn)
+
         layout.addToFooter(
             Widgets.button().withSize(min(layout.width, 200), 20).withTexture(UIConstants.BUTTON).withCallback { this.onClose() }.withRenderer(
                 WidgetRenderers.text(CommonComponents.GUI_DONE)
@@ -54,16 +58,12 @@ class GlobalConfigScreen(val previousScreen: Screen? = null) : Screen(Text.trans
     }
 
     override fun onClose() {
-        OrbitConfig.CONFIG.buttonCount = slider.value.toInt()
+        OrbitConfig.CONFIG.buttonCount = buttonCountWidget.value.toInt()
         McClient.setScreen(previousScreen)
         OrbitConfig.saveAndLoad()
     }
 
     override fun repositionElements() {
         layout.arrangeElements()
-    }
-
-    override fun renderBackground(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
-        super.renderBackground(guiGraphics, mouseX, mouseY, partialTick)
     }
 }
